20d559b4b09bcb7c601881239e9c08948831afb0
Lucas da Luz <lucasdaluz0399@gmail.com>
2021-04-25 15:17:29 -0300

    style hover select, textarea, cards, buttons. style horizontal scrollbar

--- src/components/CategoryCard/index.tsx
+++ src/components/CategoryCard/index.tsx
@@ -6,90 +6,90 @@ import TaskList from "../TaskList";
 import styles from "./categoryCard.module.scss";
 
 type CategoryCardProps = {
-	category: Category;
-	taskList: Task[];
-	handleAddCategory: () => void;
-	handleChangeCategoryTitle: (
-		id: number,
-		title: string,
-		isNew: boolean
-	) => void;
-	handleDeleteCategory: (id: number) => void;
-	handleDeleteTask: (id: number) => void;
+  category: Category;
+  taskList: Task[];
+  handleAddCategory: () => void;
+  handleChangeCategoryTitle: (
+    id: number,
+    title: string,
+    isNew: boolean
+  ) => void;
+  handleDeleteCategory: (id: number) => void;
+  handleDeleteTask: (id: number) => void;
 };
 
 export default function CategoryCard(props: CategoryCardProps) {
-	const id = props.category.id;
-	const [title, setTitle] = useState(props.category.title);
-	let isNew = props.category.isNew;
+  const id = props.category.id;
+  const [title, setTitle] = useState(props.category.title);
+  let isNew = props.category.isNew;
 
-	const _handleChangeTitle = (value: string): void => setTitle(value);
+  const _handleChangeTitle = (value: string): void => setTitle(value);
 
-	const _handleDeleteCategory = () => props.handleDeleteCategory(id);
+  const _handleDeleteCategory = () => props.handleDeleteCategory(id);
 
-	const _handleInputBlurOrSubmit = (): void => {
-		if (title === "") {
-			if (isNew) _handleDeleteCategory();
-			else setTitle(props.category.title);
-			return;
-		} else {
-			changeTitle();
-		}
-	};
+  const _handleInputBlurOrSubmit = (): void => {
+    if (title === "") {
+      if (isNew) _handleDeleteCategory();
+      else setTitle(props.category.title);
+      return;
+    } else {
+      changeTitle();
+    }
+  };
 
-	const changeTitle = (): void => {
-		if (isNew) {
-			isNew = false;
-			props.handleAddCategory();
-		}
-		props.handleChangeCategoryTitle(id, title, isNew);
-	};
+  const changeTitle = (): void => {
+    if (isNew) {
+      isNew = false;
+      props.handleAddCategory();
+    }
+    props.handleChangeCategoryTitle(id, title, isNew);
+  };
 
-	useEffect(() => {
-		let element: NodeListOf<HTMLInputElement> = document.querySelectorAll(
-			".categoryTitle"
-		);
-		if (element.length > 0 && element[element.length - 1].value === "") {
-			element[element.length - 1].focus();
-		}
-	}, []);
+  useEffect(() => {
+    let element: NodeListOf<HTMLInputElement> = document.querySelectorAll(
+      ".categoryTitle"
+    );
+    if (element.length > 0 && element[element.length - 1].value === "") {
+      element[element.length - 1].focus();
+    }
+  }, []);
 
-	return (
-		<li className={styles.categoryCard}>
-			<button
-				type="button"
-				onClick={(event) => {
-					event.preventDefault();
-					_handleDeleteCategory();
-				}}
-			>
-				<DeleteSVG />
-			</button>
-			<form
-				onSubmit={(event) => {
-					event.preventDefault();
-					event.stopPropagation();
-					if (title !== "") _handleInputBlurOrSubmit();
-				}}
-			>
-				<input
-					className="categoryTitle"
-					value={title}
-					onChange={(event) => {
-						event.preventDefault();
-						_handleChangeTitle(event.target.value);
-					}}
-					onBlur={(event) => {
-						event.preventDefault();
-						_handleInputBlurOrSubmit();
-					}}
-				/>
-			</form>
-			<TaskList
-				idCategory={props.category.id}
-				taskList={props.taskList}
-				handleDeleteTask={props.handleDeleteTask}
-			/>
-		</li>
-	);
+  return (
+    <li className={styles.categoryCard}>
+      <button
+        type="button"
+        onClick={(event) => {
+          event.preventDefault();
+          _handleDeleteCategory();
+        }}
+      >
+        <DeleteSVG />
+      </button>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          event.stopPropagation();
+          if (title !== "") _handleInputBlurOrSubmit();
+        }}
+      >
+        <input
+          className="categoryTitle"
+          value={title}
+          onChange={(event) => {
+            event.preventDefault();
+            _handleChangeTitle(event.target.value);
+          }}
+          onBlur={(event) => {
+            event.preventDefault();
+            _handleInputBlurOrSubmit();
+          }}
+        />
+      </form>
+      <TaskList
+        idCategory={props.category.id}
+        taskList={props.taskList}
+        handleDeleteTask={props.handleDeleteTask}
+      />
+    </li>
+  );
 }
